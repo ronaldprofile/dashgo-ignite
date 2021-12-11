e4e71190c270d98791528ad994bd2a512e826ecd
Ronald Tomaz <ronaldtmprofile@gmail.com>
2021-12-11 11:43:48 -0300

    I created the SignIn page using Chakra elements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,27 @@
-export default function Home() {
-  return <h1>Hello Chakra UI</h1>;
+import { Flex, Button, Stack } from "@chakra-ui/react";
+import { Input } from "../components/Form/Input";
+
+export default function SignIn() {
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxW={360}
+        direction="column"
+        bg="gray.800"
+        p="8" // % por 4 ou * 4
+        borderRadius={8}
+      >
+        <Stack spacing="4">
+          <Input name="email" type="email" label="E-mail" />
+          <Input name="password" type="password" label="Senha" />
+        </Stack>
+
+        <Button type="submit" mt="6" size="lg" colorScheme="pink">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
